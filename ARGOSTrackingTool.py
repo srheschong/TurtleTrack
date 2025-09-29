3a858d9f9a85811af805c919746fdc655f1f1ac2
srheschong <sh808@duke.edu>
2025-09-29 09:51:47 -0400

    Read data from file and process

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -8,3 +8,31 @@
 # Author: Summer Heschong (sh808@duke.edu)
 # Date:   Fall 2025
 #--------------------------------------------------------------
+
+#Create a variable pointing to the data file
+file_name = './data/raw/Sara.txt'
+
+#Create a file object from the file
+file_object = open(file_name,'r')
+
+#Read contents of file into a list
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
+
+#Pretend we read one line of data from the file
+lineString = line_list[200]
+
+#Split the string into a list of data items
+lineData = lineString.split()
+
+#Extract items in list into variables
+record_id = lineData[0]
+obs_date = lineData[2]
+obs_lc = lineData[4]
+obs_lat = lineData[6]
+obs_lon = lineData[7]
+
+#Print the location of sara
+print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
